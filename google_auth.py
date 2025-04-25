4b37b4924d93686457fa3721b1e2869e28b94d18
warleyalisson <warleyalisson@gmail.com>
2025-04-25 10:59:03 -0300

    google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -3,16 +3,16 @@ import json
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
-def conectar_planilha(sheet_name="Mapa Araruta - PANC (colaborativo)", worksheet_name="DB_mapa"):
+def conectar_planilha(sheet_id="1anS4eByA0hTI4w_spDIDPS3P205czV2f74N63UvOioM", worksheet_name="DB_mapa"):
     """
-    Conecta a uma planilha do Google Sheets e retorna a aba (worksheet) desejada.
+    Conecta a uma planilha do Google Sheets usando o ID exclusivo e retorna a aba (worksheet) especificada.
 
     Parâmetros:
-    - sheet_name: nome exato da planilha no Google Drive.
-    - worksheet_name: nome exato da aba (guia) da planilha.
+    - sheet_id: ID exclusivo da planilha no Google Sheets.
+    - worksheet_name: Nome da aba (worksheet) dentro da planilha.
 
     Retorna:
-    - worksheet: objeto da aba conectada para leitura/escrita de dados.
+    - worksheet: Objeto da aba conectada para operações de leitura/escrita.
     """
 
     escopos = [
@@ -21,14 +21,14 @@ def conectar_planilha(sheet_name="Mapa Araruta - PANC (colaborativo)", worksheet
         "https://www.googleapis.com/auth/drive"
     ]
 
-    # Carrega as credenciais do secret configurado no Streamlit Cloud
+    # Carrega as credenciais do segredo configurado no Streamlit Cloud
     creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 
-    # Cria as credenciais e cliente gspread
+    # Cria as credenciais e autoriza o cliente gspread
     credenciais = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, escopos)
     cliente = gspread.authorize(credenciais)
 
-    # Abre a planilha e retorna a worksheet desejada
-    planilha = cliente.open(sheet_name)
+    # Abre a planilha pelo ID e retorna a aba especificada
+    planilha = cliente.open_by_key(sheet_id)
     worksheet = planilha.worksheet(worksheet_name)
     return worksheet
